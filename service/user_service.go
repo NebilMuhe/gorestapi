d9d6df3ebec7ca51d26965466eb427bd275aff6e
Nebil Muhidin <nebilmuhidin57@gmail.com>
2024-04-14 10:12:33 +0300

    Add user model

--- service/user_service.go
+++ service/user_service.go
@@ -4,6 +4,14 @@ import (
 	"github.com/joho/godotenv"
 )
 
+type (
+	User struct {
+		Username string `json:"username,omitempty"`
+		Email    string `json:"email,omitempty"`
+		Password string `json:"password,omitempty"`
+	}
+)
+
 func LoadEnv() error {
 	err := godotenv.Load(".env")
 	if err != nil {
