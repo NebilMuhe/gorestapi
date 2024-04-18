41e584df84e49b2e179da20afe184235dd598dfd
Nebil Muhidin <nebilmuhidin57@gmail.com>
2024-04-18 05:10:20 +0300

    add godog feautres

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module gitlab.com/Nebil
 go 1.21.8
 
 require (
+	github.com/cucumber/godog v0.14.0
 	github.com/gin-gonic/gin v1.9.1
 	github.com/go-ozzo/ozzo-validation v3.6.0+incompatible
 	github.com/golang-jwt/jwt v3.2.2+incompatible
@@ -18,12 +19,18 @@ require (
 	github.com/asaskevich/govalidator v0.0.0-20200108200545-475eaeb16496 // indirect
 	github.com/bytedance/sonic v1.9.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
+	github.com/cucumber/gherkin/go/v26 v26.2.0 // indirect
+	github.com/cucumber/messages/go/v21 v21.0.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/gofrs/uuid v4.3.1+incompatible // indirect
+	github.com/hashicorp/go-immutable-radix v1.3.1 // indirect
+	github.com/hashicorp/go-memdb v1.3.4 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
@@ -31,6 +38,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
---
+++ godogs_test.go
@@ -0,0 +1,408 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strings"
+	"testing"
+	"unicode"
+
+	"github.com/cucumber/godog"
+)
+
+type UserRegistration struct {
+	username     string
+	email        string
+	password     string
+	errorMessage string
+}
+
+var registeredUsers = map[string]struct{}{"testuser": {}}
+
+func isValidPassword(s string) bool {
+	var (
+		hasMinLen  = false
+		hasUpper   = false
+		hasLower   = false
+		hasNumber  = false
+		hasSpecial = false
+	)
+	if len(s) >= 8 {
+		hasMinLen = true
+	}
+	for _, char := range s {
+		switch {
+		case unicode.IsUpper(char):
+			hasUpper = true
+		case unicode.IsLower(char):
+			hasLower = true
+		case unicode.IsNumber(char):
+			hasNumber = true
+		case unicode.IsPunct(char) || unicode.IsSymbol(char):
+			hasSpecial = true
+		}
+	}
+	return hasMinLen && hasUpper && hasLower && hasNumber && hasSpecial
+}
+
+func isValidUsername(s string) bool {
+	return len(s) >= 5
+}
+
+func (u *UserRegistration) aUserWithTheUsernameIsAlreadyRegistered(username string) error {
+	user := map[string]string{
+		"username": username,
+		"email":    "abc123@gmail.com",
+		"password": "12ABcd%^",
+	}
+
+	req, err := json.Marshal(user)
+	if err != nil {
+		return err
+	}
+
+	res, err := http.Post("http://localhost:8000/api/register", "application/json", strings.NewReader(string(req)))
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusCreated {
+		u.errorMessage = "username already exists"
+		return nil
+	}
+
+	return errors.New("username not exists")
+}
+
+func (u *UserRegistration) iAttemptToRegisterWithTheSameUsername() error {
+	if u.errorMessage == "username already exists" {
+		return nil
+	}
+	return errors.New("username does not exist")
+}
+
+func (u *UserRegistration) theSystemShouldReturnAnErrorMessageIndicatingThatThe() error {
+	if u.errorMessage != "username already exists" {
+		return errors.New("username doesn't exist")
+	}
+	return nil
+}
+
+func (u *UserRegistration) iAmRegisteringWithAnInvalidEmailFormat() error {
+	user := map[string]string{
+		"username": "testuser",
+		"email":    "abc123gmail.com",
+		"password": "12ABcd%^",
+	}
+
+	req, err := json.Marshal(user)
+	if err != nil {
+		return err
+	}
+
+	res, err := http.Post("http://localhost:8000/api/register", "application/json", strings.NewReader(string(req)))
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode == http.StatusBadRequest {
+		u.errorMessage = "invalid email format"
+		return nil
+	}
+
+	return errors.New("valid email")
+}
+
+func (u *UserRegistration) iSubmitTheRegistrationForm() error {
+	return nil
+}
+
+func (u *UserRegistration) theSystemShouldReturnAnErrorMessageIndicatingThatTheEmailFormatIsInvalid() error {
+	if u.errorMessage == "invalid email format" {
+		return nil
+	}
+	return errors.New("email format is valid")
+}
+
+func (u *UserRegistration) iAmRegisteringWithAWeakPassword() error {
+	user := map[string]string{
+		"username": "testuser",
+		"email":    "abc123@gmail.com",
+		"password": "12345678",
+	}
+	req, err := json.Marshal(user)
+	if err != nil {
+		return err
+	}
+
+	res, err := http.Post("http://localhost:8000/api/register", "application/json", strings.NewReader(string(req)))
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode == http.StatusBadRequest {
+		u.errorMessage = "the password is not strong enough"
+		return nil
+	}
+	return errors.New("strong password")
+}
+
+func (u *UserRegistration) theSystemShouldReturnAnErrorMessageIndicatingThatThePasswordIsNotStrongEnough() error {
+	if u.errorMessage == "the password is not strong enough" {
+		return nil
+	}
+	return errors.New("password is strong enough")
+}
+
+func (u *UserRegistration) iAmRegisteringWithAUsernameLessThanCharactersLong(arg1 int) error {
+	user := map[string]string{
+		"username": "test",
+		"email":    "abc123@gmail.com",
+		"password": "12345678",
+	}
+	req, err := json.Marshal(user)
+	if err != nil {
+		return err
+	}
+
+	res, err := http.Post("http://localhost:8000/api/register", "application/json", strings.NewReader(string(req)))
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode == http.StatusBadRequest {
+		u.errorMessage = "the username must be at least characters long"
+		return nil
+	}
+	return errors.New("valid username")
+}
+
+func (u *UserRegistration) theSystemShouldReturnAnErrorMessageIndicatingThatTheUsernameMustBeAtLeastCharactersLong(minLength int) error {
+	if u.errorMessage == "the username must be at least characters long" {
+		return nil
+	}
+	return errors.New("username has more than 5 characters long")
+}
+
+func (u *UserRegistration) iAmRegisteringWithAPasswordThatDoesNotMeetTheStrengthRequirements() error {
+	user := map[string]string{
+		"username": "testuser",
+		"email":    "abc123@gmail.com",
+		"password": "12345tghik",
+	}
+	req, err := json.Marshal(user)
+	if err != nil {
+		return err
+	}
+
+	res, err := http.Post("http://localhost:8000/api/register", "application/json", strings.NewReader(string(req)))
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode == http.StatusBadRequest {
+		u.errorMessage = "at least 8 characters long, with at least one uppercase letter, one lowercase letter, one digit, and one special character"
+		return nil
+	}
+
+	return errors.New("valid password")
+}
+
+func (u *UserRegistration) theSystemShouldReturnAnErrorMessageIndicatingThePasswordRequirementsEgAtLeastCharactersLongWithAtLeastOneUppercaseLetterOneLowercaseLetterOneDigitAndOneSpecialCharacter(length int) error {
+	if u.errorMessage == "at least 8 characters long, with at least one uppercase letter, one lowercase letter, one digit, and one special character" {
+		return nil
+	}
+	return errors.New("strong password")
+}
+
+func (u *UserRegistration) iAmARegisteredUserWithValidCredentials() error {
+	user := map[string]string{
+		"username": "nebil12",
+		"email":    "nebil@gmail.com",
+		"password": "1234ABcd%^",
+	}
+	req, err := json.Marshal(user)
+	if err != nil {
+		return err
+	}
+
+	res, err := http.Post("http://localhost:8000/api/register", "application/json", strings.NewReader(string(req)))
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode == http.StatusCreated {
+		u.errorMessage = "user registered"
+		return nil
+	}
+	return errors.New("unable to register user")
+}
+
+func (u *UserRegistration) iLogInWithMyUsernameAndPassword() error {
+	if u.errorMessage == "user registered" {
+		user := map[string]string{
+			"username": "nebil12",
+			"password": "1234ABcd%^",
+		}
+		req, err := json.Marshal(user)
+		if err != nil {
+			return err
+		}
+
+		res, err := http.Post("http://localhost:8000/api/login", "application/json", strings.NewReader(string(req)))
+		if err != nil {
+			return err
+		}
+		defer res.Body.Close()
+
+		if res.StatusCode == http.StatusOK {
+			u.errorMessage = "access token and refresh token generated"
+			return nil
+		}
+	}
+	return errors.New("unable to login user")
+}
+
+func (u *UserRegistration) theSystemShouldGenerateAJWTTokenForAuthenticationAndIssueARefreshToken() error {
+	if u.errorMessage == "access token and refresh token generated" {
+		return nil
+	}
+	return errors.New("unable to generate refresh token")
+}
+
+func (u *UserRegistration) iAmAttemptingToLogInWithAnInvalidUsername() error {
+	user := map[string]string{
+		"username": "nebil10",
+		"password": "1234ABcd%^",
+	}
+	req, err := json.Marshal(user)
+	if err != nil {
+		return err
+	}
+
+	res, err := http.Post("http://localhost:8000/api/login", "application/json", strings.NewReader(string(req)))
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	if res.StatusCode == http.StatusNotFound {
+		u.errorMessage = "the username is not registered"
+		return nil
+	}
+	return errors.New("username registered")
+}
+
+func (u *UserRegistration) iSubmitTheLoginForm() error {
+	return nil
+}
+
+func (u *UserRegistration) theSystemShouldReturnAnErrorMessageIndicatingThatTheUsernameIsNotRegistered() error {
+	if u.errorMessage == "the username is not registered" {
+		return nil
+	}
+	return errors.New("user already reigtered")
+}
+
+func (u *UserRegistration) iAmAttemptingToLogInWithAnInvalidPassword() error {
+	user := map[string]string{
+		"username": "nebil12",
+		"password": "1234ABcd$%",
+	}
+	req, err := json.Marshal(user)
+	if err != nil {
+		return err
+	}
+
+	res, err := http.Post("http://localhost:8000/api/login", "application/json", strings.NewReader(string(req)))
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode == http.StatusBadRequest {
+		u.errorMessage = "the password is incorrect"
+		return nil
+	}
+	return errors.New("password is correct")
+}
+
+func (u *UserRegistration) theSystemShouldReturnAnErrorMessageIndicatingThatThePasswordIsIncorrect() error {
+	if u.errorMessage == "the password is incorrect" {
+		return nil
+	}
+	return errors.New("password is correct")
+}
+
+func InitializeScenario(ctx *godog.ScenarioContext) {
+	user := &UserRegistration{}
+	ctx.Step(`^a user with the username "([^"]*)" is already registered,$`,
+		user.aUserWithTheUsernameIsAlreadyRegistered)
+	ctx.Step(`^I attempt to register with the same username,$`,
+		user.iAttemptToRegisterWithTheSameUsername)
+	ctx.Step(`^the system should return an error message indicating that the username already exists.$`,
+		user.theSystemShouldReturnAnErrorMessageIndicatingThatThe)
+
+	ctx.Step(`^I am registering with an invalid email format,$`,
+		user.iAmRegisteringWithAnInvalidEmailFormat)
+	ctx.Step(`^I submit the registration form,$`,
+		user.iSubmitTheRegistrationForm)
+	ctx.Step(`^the system should return an error message indicating that the email format is invalid\.$`,
+		user.theSystemShouldReturnAnErrorMessageIndicatingThatTheEmailFormatIsInvalid)
+
+	ctx.Step(`^I am registering with a weak password,$`,
+		user.iAmRegisteringWithAWeakPassword)
+	ctx.Step(`^the system should return an error message indicating that the password is not strong enough\.$`,
+		user.theSystemShouldReturnAnErrorMessageIndicatingThatThePasswordIsNotStrongEnough)
+
+	ctx.Step(`^I am registering with a username less than (\d+) characters long,$`,
+		user.iAmRegisteringWithAUsernameLessThanCharactersLong)
+	ctx.Step(`^the system should return an error message indicating that the username must be at least (\d+) characters long\.$`,
+		user.theSystemShouldReturnAnErrorMessageIndicatingThatTheUsernameMustBeAtLeastCharactersLong)
+
+	ctx.Step(`^I am registering with a password that does not meet the strength requirements,$`,
+		user.iAmRegisteringWithAPasswordThatDoesNotMeetTheStrengthRequirements)
+	ctx.Step(`^the system should return an error message indicating the password requirements \(e\.g\., at least (\d+) characters long, with at least one uppercase letter, one lowercase letter, one digit, and one special character\)\.$`,
+		user.theSystemShouldReturnAnErrorMessageIndicatingThePasswordRequirementsEgAtLeastCharactersLongWithAtLeastOneUppercaseLetterOneLowercaseLetterOneDigitAndOneSpecialCharacter)
+
+	ctx.Step(`^I am a registered user with valid credentials,$`,
+		user.iAmARegisteredUserWithValidCredentials)
+	ctx.Step(`^I log in with my username and password,$`,
+		user.iLogInWithMyUsernameAndPassword)
+	ctx.Step(`^the system should generate a JWT token for authentication and issue a refresh token$`,
+		user.theSystemShouldGenerateAJWTTokenForAuthenticationAndIssueARefreshToken)
+
+	ctx.Step(`^I am attempting to log in with an invalid username,$`,
+		user.iAmAttemptingToLogInWithAnInvalidUsername)
+	ctx.Step(`^I submit the login form,$`,
+		user.iSubmitTheLoginForm)
+	ctx.Step(`^the system should return an error message indicating that the username is not registered\.$`,
+		user.theSystemShouldReturnAnErrorMessageIndicatingThatTheUsernameIsNotRegistered)
+
+	ctx.Step(`^I am attempting to log in with an invalid password,$`,
+		user.iAmAttemptingToLogInWithAnInvalidPassword)
+	ctx.Step(`^the system should return an error message indicating that the password is incorrect\.$`,
+		user.theSystemShouldReturnAnErrorMessageIndicatingThatThePasswordIsIncorrect)
+
+}
+
+func TestFeatures(t *testing.T) {
+	suite := godog.TestSuite{
+		ScenarioInitializer: InitializeScenario,
+		Options: &godog.Options{
+			Format:   "pretty",
+			Paths:    []string{"features"},
+			TestingT: t, // Testing instance that will run subtests.
+		},
+	}
+
+	if suite.Run() != 0 {
+		t.Fatal("non-zero status returned, failed to run feature tests")
+	}
+}
